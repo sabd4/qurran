b5ef731522f4c73bfb1ea5c516aa1fbe929739bd
Sondos Abo Shaweesh <sabd41078@gmail.com>
2023-08-01 15:31:32 +0300

    qurran
    qurran_app

---
+++ android/app/src/main/kotlin/com/example/qurran/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qurran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:qurran/screen/home_page_screen.dart';
+import 'package:qurran/screen/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => MyHomePage(),
+      },
+    );
+  }
+}
